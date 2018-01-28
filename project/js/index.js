9b643a5b60a3a800c717d72422b1c1bf84a7f6d4
lglash <lglash@hotmail.co.uk>
2018-01-28 14:10:11 +0800

    ok

---
+++ project/js/index.js
@@ -0,0 +1,9 @@
+//this is the log message to console to verify that JS file is loaded//
+
+ console.log ('javascript file linked to index.html');
+
+ //this below is for JS to execute only after all elements have rendered on the page//
+ $(document).ready(function(){
+ 	//this below is the log message to the console to verify JQuery has loaded//
+ 	console.log('jquery is loaded');
+

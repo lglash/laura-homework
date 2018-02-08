01f102d7baae67e0b88462f723e5050cd1552b78
lglash <lglash@hotmail.co.uk>
2018-02-08 10:36:25 +0800

    ok

--- project/js/index.js
+++ project/js/index.js
@@ -7,18 +7,26 @@
  	//this below is the log message to the console to verify JQuery has loaded//
  	console.log('jquery is loaded');
 
+//TEAM//
+
+//direct children of directors (top 3 images). When hovering over each image, toggle between showing hidden div/hiding it.
   $(".directors > div").each( function() {
     $(this).hover(function() {
       $(this).toggleClass("hover");
     });
     
   });
-
+//direct children of minions (bottom 3 images). When hovering over each image, toggle between showing hidden div/hiding it.
    $(".minions > div").each( function() {
     $(this).hover(function() {
       $(this).toggleClass("hover");
     });
     
   });
+
+//FAQs//
+
+
+
   
 });

af736dae033d66537eefdcfeb6383d49273d5f39
lglash <lglash@hotmail.co.uk>
2018-02-08 23:05:59 +0800

    added contact form

--- project/js/index.js
+++ project/js/index.js
@@ -7,6 +7,8 @@
  	//this below is the log message to the console to verify JQuery has loaded//
  	console.log('jquery is loaded');
 
+
+
 //TEAM//
 
 //direct children of directors (top 3 images). When hovering over each image, toggle between showing hidden div/hiding it.
